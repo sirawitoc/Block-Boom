10aa53912082c5493361bc377b812832a5fd9ac6
nattavech-lab <nattavech.k@ku.th>
2026-09-23 17:43:48 +0700

    Add files via upload

---
+++ model/Board.java
@@ -0,0 +1,9 @@
+package model;
+import  java.awt.Color;
+
+
+public  class Board {
+
+     public  static final int SIZE=8;
+     public  Color[][] grid;  //ช่องว่างมีค่า=มีบล็อกอยู่ (เก้บสีขอบล็อกนั้น)
+}
