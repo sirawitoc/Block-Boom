952f62d420cee2115e6415a6eabe5085ad8faee2
thanakritxe <thanakrit.o@ku.th>
2026-09-23 16:35:20 +0700

    Add files via upload

---
+++ ScoreEntry.java
@@ -0,0 +1,15 @@
+public class ScoreEntry  {
+    private String name;
+    private int score;
+
+    public String getName(){
+        return name;
+        
+    }
+
+    public int getScore(){
+        return score;
+        
+    }
+
+}
